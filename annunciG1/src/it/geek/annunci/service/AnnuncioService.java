76b73e3a66b0290451ef2856ed6131a6b1ac6352
Riccardo Carnazza <riccardo.carnazza@gmail.com>
2013-05-20 13:49:08 +0200

    service

---
+++ annunciG1/src/it/geek/annunci/service/AnnuncioService.java
@@ -0,0 +1,21 @@
+package it.geek.annunci.service;
+
+import it.geek.annunci.model.Annuncio;
+
+import java.util.List;
+
+public class AnnuncioService implements AnnuncioServiceInterface {
+
+	@Override
+	public List<Annuncio> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean getExample(Annuncio u) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ annunciG1/src/it/geek/annunci/service/ServiceFactory.java
@@ -0,0 +1,17 @@
+package it.geek.annunci.service;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ServiceFactory {
+	
+	private static ApplicationContext cxt = null;
+
+	static{
+		cxt = new ClassPathXmlApplicationContext("applicationContext.xml");
+	}
+	
+	public static AnnuncioServiceInterface getAnnuncioService(){
+		return (AnnuncioServiceInterface)cxt.getBean("aservice");
+	}
+}
